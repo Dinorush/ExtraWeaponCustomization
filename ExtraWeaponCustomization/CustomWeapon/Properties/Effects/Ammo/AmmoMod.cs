41b4102e4fa1960b4a7aa70808c0af20fe94fe35
Dinorush <onparnorthstar@gmail.com>
2024-05-20 20:02:55 -0400

    Fix mag exceeding normal values
    Could occur if a trigger effect ran when the gun didn't fire (e.g. DOT)

--- ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Ammo/AmmoMod.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Ammo/AmmoMod.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects
 {
     public sealed class AmmoMod :
+        IWeaponProperty<WeaponPreFireContext>,
         IWeaponProperty<WeaponTriggerContext>
     {
         public readonly static string Name = typeof(AmmoMod).Name;
@@ -18,6 +19,12 @@ namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects
 
         private float _clipBuffer = 0;
         private float _reserveBuffer = 0;
+        private float _lastFireTime = 0;
+
+        public void Invoke(WeaponPreFireContext context)
+        {
+            _lastFireTime = Clock.Time;
+        }
 
         public void Invoke(WeaponTriggerContext context)
         {
@@ -27,11 +34,15 @@ namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects
             _reserveBuffer += ReserveChange;
 
             if (_clipBuffer < 1 && _clipBuffer > -1 && _reserveBuffer < 1 && _reserveBuffer > -1) return;
+            
+            // Ammo decrements after this callback if on kill/shot/hit, need to account for that.
+            // But if this weapon didn't get the kill (e.g. DOT kill), shouldn't do that.
+            int accountForShot = Clock.Time == _lastFireTime ? 1 : 0;
 
             // Calculate the actual changes we can make to clip/ammo
             PlayerAmmoStorage ammoStorage = PlayerBackpackManager.GetBackpack(context.Weapon.Owner.Owner).AmmoStorage;
             int clipChange = (int) (PullFromReserve ? Math.Min(_clipBuffer, ammoStorage.GetBulletsInPack(context.Weapon.AmmoType)) : _clipBuffer);
-            int newClip = Math.Clamp(context.Weapon.GetCurrentClip() + clipChange, 0, context.Weapon.GetMaxClip() + 1);
+            int newClip = Math.Clamp(context.Weapon.GetCurrentClip() + clipChange, accountForShot, context.Weapon.GetMaxClip() + accountForShot);
             clipChange = newClip - context.Weapon.GetCurrentClip();
             int reserveChange = (int) (PullFromReserve ? _reserveBuffer - clipChange : _reserveBuffer);
 
