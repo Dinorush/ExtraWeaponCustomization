43713463563b4a98f6fe42afea846d2969bf63db
Dinorush <onparnorthstar@gmail.com>
2024-05-21 20:50:49 -0400

    Fix null error on drop

--- ExtraWeaponCustomization/Patches/PlayerLocalPatches.cs
+++ ExtraWeaponCustomization/Patches/PlayerLocalPatches.cs
@@ -10,7 +10,7 @@ namespace ExtraWeaponCustomization.Patches
         [HarmonyPrefix]
         private static void UpdateHealth(PUI_LocalPlayerStatus __instance, float health, bool meleeBuffActive)
         {
-            if (__instance.m_lastHealthVal <= 0.14f && health > 0.14f)
+            if (__instance.m_lastHealthVal <= 0.14f && health > 0.14f && __instance.m_warningRoutine != null)
                 __instance.m_healthWarningLooping = true;
         }
     }
