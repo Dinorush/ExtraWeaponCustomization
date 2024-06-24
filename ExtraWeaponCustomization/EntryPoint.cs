1388beaef2d181a562a236b4a918265ddc3ce412
Dinorush <onparnorthstar@gmail.com>
2024-06-24 17:10:36 -0400

    Version bump

--- ExtraWeaponCustomization/EntryPoint.cs
+++ ExtraWeaponCustomization/EntryPoint.cs
@@ -12,7 +12,7 @@ using ExtraWeaponCustomization.CustomWeapon.Properties.Effects;
 
 namespace ExtraWeaponCustomization;
 
-[BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.4.3")]
+[BepInPlugin("Dinorush." + MODNAME, MODNAME, "1.4.4")]
 [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MTFOUtil.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MTFOPartialDataUtil.PLUGIN_GUID, BepInDependency.DependencyFlags.SoftDependency)]
