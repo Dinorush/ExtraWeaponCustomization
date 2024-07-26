4abd3728b82a9f3730780d72e4f2d42352bec332
Dinorush <onparnorthstar@gmail.com>
2024-07-26 14:26:38 -0400

    Push WIP projectile work

---
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Traits/Projectile/ProjectileComponentBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace ExtraWeaponCustomization.CustomWeapon.Properties.Traits
+{
+    public abstract class ProjectileComponentBase : MonoBehaviour
+    {
+        public ProjectileComponentBase(IntPtr ptr) : base(ptr) { }
+
+        protected abstract Vector3 Velocity { get; set; }
+        protected abstract Vector3 Position { get; }
+        protected float _Gravity;
+
+        public void Init(Vector3 velocity, float gravity)
+        {
+            Velocity = velocity;
+            _Gravity = gravity;
+        }
+    }
+}
