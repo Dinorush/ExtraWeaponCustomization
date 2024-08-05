3d585bde4e5dcb2d70c817eea5147b6ec73a9c71
Dinorush <onparnorthstar@gmail.com>
2024-08-05 15:49:29 -0400

    Add Cap field & fix apply error
    Apply activations could trigger even when accumulated triggers were empty, causing an error.

--- ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerCoordinator.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerCoordinator.cs
@@ -10,6 +10,7 @@ namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects.Triggers
         private static readonly Random Random = new();
         public ITriggerCallback? Parent { get; set; }
         public List<ITrigger> Activate { get; set; } = new();
+        public uint Cap { get; set; } = 0;
         public float Cooldown { get; set; } = 0f;
         public float Chance { get; set; } = 1f;
 
@@ -34,6 +35,7 @@ namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects.Triggers
                 Activate = new(Activate),
                 Apply = Apply != null ? new(Apply) : null,
                 Reset = Reset != null ? new(Reset) : null,
+                Cap = Cap,
                 Cooldown = Cooldown,
                 Chance = Chance,
                 CooldownOnApply = CooldownOnApply
@@ -44,7 +46,9 @@ namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects.Triggers
         public void Invoke(WeaponTriggerContext context)
         {
             // Store valid activations (if any)
-            if (Clock.Time >= _nextActivateTime && (Chance == 1f || Chance > Random.NextDouble()))
+            if (Clock.Time >= _nextActivateTime
+             && (Cap == 0 || _accumulatedTriggers.Count < Cap)
+             && (Chance == 1f || Chance > Random.NextDouble()))
             {
                 foreach (ITrigger trigger in Activate)
                 {
@@ -53,12 +57,13 @@ namespace ExtraWeaponCustomization.CustomWeapon.Properties.Effects.Triggers
                     {
                         _accumulatedTriggers.Add(new TriggerContext { triggerAmt = triggerAmt, context = context });
                         _nextActivateTime = Math.Max(_nextActivateTime, Clock.Time + Cooldown);
+                        if (Cap > 0 && _accumulatedTriggers.Count == Cap) break;
                     }
                 }
             }
 
             // Apply stored activations. If there are no Apply triggers, stored activations apply immediately
-            if (Apply?.Any(trigger => trigger.Invoke(context) > 0) ?? _accumulatedTriggers.Any() == true)
+            if (_accumulatedTriggers.Any() && (Apply?.Any(trigger => trigger.Invoke(context) > 0) ?? true))
                 ApplyTriggers();
 
             // Reset stored activations AND any related behavior on the callback this coordinator is tied to
