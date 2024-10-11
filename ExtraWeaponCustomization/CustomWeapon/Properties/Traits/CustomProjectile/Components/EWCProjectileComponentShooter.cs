f7fe1787998a36eeda4d3d6a0a596a1abe483d63
Dinorush <onparnorthstar@gmail.com>
2024-10-11 09:21:00 -0400

    Fix projectile speed

--- ExtraWeaponCustomization/CustomWeapon/Properties/Traits/CustomProjectile/Components/EWCProjectileComponentShooter.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Traits/CustomProjectile/Components/EWCProjectileComponentShooter.cs
@@ -13,6 +13,7 @@ namespace EWC.CustomWeapon.Properties.Traits.CustomProjectile.Components
 
         private Color _origGlowColor;
         private float _origGlowRange;
+        private Vector3 _baseVelocity;
 
 #pragma warning disable CS8618
         public EWCProjectileComponentShooter(IntPtr ptr) : base(ptr) { }
@@ -23,6 +24,7 @@ namespace EWC.CustomWeapon.Properties.Traits.CustomProjectile.Components
             if (enabled) return;
 
             base.Init(ID, position, velocity, accel, accelExpo, accelTime, gravity, scale, lifetime, sendDestroy);
+            _baseVelocity = velocity;
 
             _trailRenderer?.Clear();
 
@@ -78,10 +80,10 @@ namespace EWC.CustomWeapon.Properties.Traits.CustomProjectile.Components
             if (_accel != 1f)
             {
                 _accelProgress = Math.Min(_accelProgress + Time.deltaTime / _accelTime, 1f);
-                _velocity = _baseDir * Mathf.Lerp(1f, _accel, (float) Math.Pow(_accelProgress, _accelExpo));
+                _velocity = _baseVelocity * Mathf.Lerp(1f, _accel, (float) Math.Pow(_accelProgress, _accelExpo));
             }
             else
-                _velocity = _baseDir;
+                _velocity = _baseVelocity;
             _velocity.y -= _gravityVel;
 
             Vector3 velocity = _velocity * Time.deltaTime;
