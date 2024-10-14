767f8a311fa27f29d226c34abe6570926f5fc91d
Dinorush <onparnorthstar@gmail.com>
2024-10-13 21:56:22 -0400

    Refactor trigger & add threshold

--- ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerCoordinator.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerCoordinator.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using EWC.CustomWeapon.WeaponContext.Contexts;
 using System.Linq;
+using System.Text.Json;
+using EWC.JSON;
 
 namespace EWC.CustomWeapon.Properties.Effects.Triggers
 {
@@ -9,22 +11,23 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
     {
         private static readonly Random Random = new();
         public ITriggerCallback? Parent { get; set; }
-        public List<ITrigger> Activate { get; set; } = new();
-        public uint Cap { get; set; } = 0;
-        public float Cooldown { get; set; } = 0f;
-        public float Chance { get; set; } = 1f;
+        public List<ITrigger> Activate { get; private set; }
+        public uint Cap { get; private set; } = 0;
+        public uint Threshold { get; private set; } = 1;
+        public float Cooldown { get; private set; } = 0f;
+        public float Chance { get; private set; } = 1f;
 
-        public List<ITrigger>? Apply { get; set; }
-        public float CooldownOnApply { get; set; } = 0f;
+        public List<ITrigger>? Apply { get; private set; }
+        public float CooldownOnApply { get; private set; } = 0f;
 
-        public List<ITrigger>? Reset { get; set; }
+        public List<ITrigger>? Reset { get; private set; }
 
         private float _nextActivateTime = 0f;
         private readonly List<TriggerContext> _accumulatedTriggers = new();
 
         public TriggerCoordinator(params ITrigger[] triggers)
         {
-            Activate.AddRange(triggers);
+            Activate = new(triggers);
         }
 
         public TriggerCoordinator Clone()
@@ -63,7 +66,7 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
             }
 
             // Apply stored activations. If there are no Apply triggers, stored activations apply immediately
-            if (_accumulatedTriggers.Any() && (Apply?.Any(trigger => trigger.Invoke(context) > 0) ?? true))
+            if (_accumulatedTriggers.Count >= Threshold && (Apply?.Any(trigger => trigger.Invoke(context) > 0) ?? true))
                 ApplyTriggers();
 
             // Reset stored activations AND any related behavior on the callback this coordinator is tied to
@@ -84,6 +87,66 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
         {
             _accumulatedTriggers.Clear();
             Parent?.TriggerReset();
+        }
+
+        public void DeserializeProperty(string property, ref Utf8JsonReader reader)
+        {
+            switch (property)
+            {
+                case "cap":
+                    Cap = reader.GetUInt32();
+                    break;
+                case "threshold":
+                    Threshold = Math.Max(reader.GetUInt32(), 1);
+                    break;
+                case "cooldown":
+                    Cooldown = reader.GetSingle();
+                    break;
+                case "chance":
+                    Chance = reader.GetSingle();
+                    break;
+                case "cooldownonapply":
+                    CooldownOnApply = reader.GetSingle();
+                    break;
+                case "activate":
+                case "triggers":
+                case "trigger":
+                    // Can actually be null here, but converter will return a null if this is.
+                    Activate = DeserializeTriggers(ref reader)!;
+                    break;
+                case "apply":
+                    Apply = DeserializeTriggers(ref reader);
+                    break;
+                case "reset":
+                    Reset = DeserializeTriggers(ref reader);
+                    break;
+            }
+        }
+
+        private static List<ITrigger>? DeserializeTriggers(ref Utf8JsonReader reader)
+        {
+            if (reader.TokenType == JsonTokenType.String || reader.TokenType == JsonTokenType.StartObject)
+            {
+                ITrigger? trigger = EWCJson.Deserialize<ITrigger>(ref reader);
+                if (trigger == null) return null;
+                return new List<ITrigger>() { trigger };
+            }
+            else if (reader.TokenType == JsonTokenType.StartArray)
+            {
+                List<ITrigger> result = new();
+                reader.Read();
+                while (reader.TokenType != JsonTokenType.EndArray)
+                {
+                    ITrigger? trigger = EWCJson.Deserialize<ITrigger>(ref reader);
+                    reader.Read();
+                    if (trigger == null) return null;
+                    result.Add(trigger);
+                }
+                if (!result.Any()) return null;
+                return result;
+            }
+
+            throw new JsonException("Expected trigger or list of triggers when deserializing triggers for Coordinator");
         }
     }
 }
--- ExtraWeaponCustomization/JSON/Converters/TriggerCoordinatorConverter.cs
+++ ExtraWeaponCustomization/JSON/Converters/TriggerCoordinatorConverter.cs
@@ -1,7 +1,5 @@
 ﻿using EWC.CustomWeapon.Properties.Effects.Triggers;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
@@ -44,68 +42,16 @@ namespace EWC.JSON.Converters
             while (reader.Read())
             {
                 if (reader.TokenType == JsonTokenType.EndObject)
-                    return coordinator.Activate.Count > 0 ? coordinator : null;
+                    return coordinator.Activate?.Count > 0 ? coordinator : null;
 
                 if (reader.TokenType != JsonTokenType.PropertyName) throw new JsonException("Expected PropertyName token");
 
                 string property = reader.GetString()!;
                 reader.Read();
-                switch (property.ToLowerInvariant().Replace(" ", ""))
-                {
-                    case "cap":
-                        coordinator.Cap = reader.GetUInt32();
-                        break;
-                    case "cooldown":
-                        coordinator.Cooldown = reader.GetSingle();
-                        break;
-                    case "chance":
-                        coordinator.Chance = reader.GetSingle();
-                        break;
-                    case "cooldownonapply":
-                        coordinator.CooldownOnApply = reader.GetSingle();
-                        break;
-                    case "activate":
-                    case "triggers":
-                    case "trigger":
-                        coordinator.Activate = DeserializeTriggers(ref reader, options);
-                        if (coordinator.Activate?.Any() != true) return null;
-                        break;
-                    case "apply":
-                        coordinator.Apply = DeserializeTriggers(ref reader, options);
-                        break;
-                    case "reset":
-                        coordinator.Reset = DeserializeTriggers(ref reader, options);
-                        break;
-                }
+                coordinator.DeserializeProperty(property.ToLowerInvariant().Replace(" ", ""), ref reader);
             }
 
             throw new JsonException("Expected EndObject token");
-        }
-
-        private static List<ITrigger>? DeserializeTriggers(ref Utf8JsonReader reader, JsonSerializerOptions options)
-        {
-            if (reader.TokenType == JsonTokenType.String || reader.TokenType == JsonTokenType.StartObject)
-            {
-                ITrigger? trigger = EWCJson.Deserialize<ITrigger>(ref reader);
-                if (trigger == null) return null;
-                return new List<ITrigger>() { trigger };
-            }
-            else if (reader.TokenType == JsonTokenType.StartArray)
-            {
-                List<ITrigger> result = new();
-                reader.Read();
-                while (reader.TokenType != JsonTokenType.EndArray)
-                {
-                    ITrigger? trigger = EWCJson.Deserialize<ITrigger>(ref reader);
-                    reader.Read();
-                    if (trigger == null) return null;
-                    result.Add(trigger);
-                }
-                if (!result.Any()) return null;
-                return result;
-            }
-
-            throw new JsonException("Expected trigger or list of triggers when deserializing triggers for Coordinator");
         }
 
         // Only called for templates, so don't need logic for customized coordinators/triggers
