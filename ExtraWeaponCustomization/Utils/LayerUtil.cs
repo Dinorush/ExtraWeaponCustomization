bb941f3310bd4de3dc30f805901c2b10fc2b0b43
Dinorush <onparnorthstar@gmail.com>
2024-10-16 21:11:41 -0400

    Add layers to remove LayerManager calls

--- ExtraWeaponCustomization/Utils/LayerUtil.cs
+++ ExtraWeaponCustomization/Utils/LayerUtil.cs
@@ -6,19 +6,35 @@ namespace EWC.Utils
     {
         public static int MaskDynamic { get; private set; }
         public static int MaskEntityAndWorld { get; private set; }
+        public static int MaskEntityAndWorld3P { get; private set; }
         public static int MaskWorld { get; private set; }
+        public static int MaskWorldExcProj { get; private set; }
+        public static int MaskDecalValid { get; private set; }
+        public static int MaskEntityDynamic3P { get; private set; }
         public static int MaskEntity { get; private set; }
+        public static int MaskEntity3P { get; private set; }
         public static int MaskOwner { get; private set; }
         public static int MaskFriendly { get; private set; }
+        public static int MaskEnemy { get; private set; }
+        public static int MaskEnemyDynamic { get; private set; }
 
+        // Has duplicates with LayerManager, want to avoid Il2Cpp overhead
         internal static void Init()
         {
             MaskOwner = LayerMask.GetMask("PlayerMover");
             MaskFriendly = LayerMask.GetMask("PlayerSynced");
-            MaskEntity = LayerMask.GetMask("PlayerMover", "PlayerSynced", "EnemyDamagable");
-            MaskWorld = LayerMask.GetMask("Default", "Default_NoGraph", "Default_BlockGraph", "ProjectileBlocker", "Dynamic");
-            MaskEntityAndWorld = MaskEntity | MaskWorld;
+            MaskEnemy = LayerMask.GetMask("EnemyDamagable");
             MaskDynamic = LayerMask.GetMask("Dynamic");
+
+            MaskEntity3P = MaskFriendly | MaskEnemy;
+            MaskEntity = MaskOwner | MaskEntity3P;
+
+            MaskDecalValid = LayerMask.GetMask("Default", "Default_NoGraph", "Default_BlockGraph");
+            MaskWorldExcProj = MaskDecalValid | MaskDynamic;
+            MaskWorld = MaskWorldExcProj | LayerMask.GetMask("ProjectileBlocker");
+            MaskEntityAndWorld = MaskEntity | MaskWorld;
+            MaskEntityDynamic3P = MaskEntity3P | MaskDynamic;
+            MaskEntityAndWorld3P = MaskEntity3P | MaskWorld;
         }
     }
 }
