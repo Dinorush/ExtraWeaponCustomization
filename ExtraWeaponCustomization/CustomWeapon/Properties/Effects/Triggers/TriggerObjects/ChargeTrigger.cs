b2f4eae88c1499e40a40ccc39c9fb931ca10ac59
Dinorush <onparnorthstar@gmail.com>
2024-11-28 13:38:51 -0500

    Correct charge formula

--- ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerObjects/ChargeTrigger.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerObjects/ChargeTrigger.cs
@@ -22,7 +22,7 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
             float charge = MeleePatches.CachedCharge;
             if (charge >= MinRequired && charge <= MaxRequired)
             {
-                charge = Math.Pow(charge.Lerp(MinRequired, MaxRequired), Exponent).Lerp(Min, Max);
+                charge = Math.Pow(charge.Map(MinRequired, MaxRequired, 0f, 1f), Exponent).Lerp(Min, Max);
                 return charge * Amount;
             }
             return 0f;
