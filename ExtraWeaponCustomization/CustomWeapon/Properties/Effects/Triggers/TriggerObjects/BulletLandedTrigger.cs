bb3592337307af9b4a9869204e10d17362369449
Dinorush <onparnorthstar@gmail.com>
2024-12-24 22:09:03 -0500

    Fix bullet landed triggers firing on non-bullet hits

--- ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerObjects/BulletLandedTrigger.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerObjects/BulletLandedTrigger.cs
@@ -1,41 +1,21 @@
 ﻿using EWC.CustomWeapon.WeaponContext.Contexts;
 using EWC.CustomWeapon.WeaponContext.Contexts.Triggers;
-using System.Text.Json;
 
 namespace EWC.CustomWeapon.Properties.Effects.Triggers
 {
-    public sealed class BulletLandedTrigger : ITrigger
+    public sealed class BulletLandedTrigger : DamageTypeTrigger<WeaponHitContextBase>
     {
-        public TriggerName Name { get; } = TriggerName.BulletLanded;
-        public float Amount { get; private set; } = 1f;
+        public BulletLandedTrigger() : base(TriggerName.BulletLanded, DamageType.Bullet) {}
 
-        public BulletLandedTrigger() {}
-
-        public bool Invoke(WeaponTriggerContext context, out float amount)
+        public override bool Invoke(WeaponTriggerContext context, out float amount)
         {
-            amount = 0f;
             // Want to trigger when a bullet lands but NOT on a pre-hit context
-            if (context is WeaponHitContextBase && context is not WeaponPreHitDamageableContext)
+            if (base.Invoke(context, out amount) && context is not WeaponPreHitDamageableContext)
             {
                 amount = Amount;
                 return true;
             }
             return false;
-        }
-
-        public void Reset() { }
-
-        public ITrigger Clone() => this;
-
-        public void DeserializeProperty(string property, ref Utf8JsonReader reader)
-        {
-            switch (property)
-            {
-                case "triggeramount":
-                case "amount":
-                    Amount = reader.GetSingle();
-                    break;
-            }
         }
     }
 }
