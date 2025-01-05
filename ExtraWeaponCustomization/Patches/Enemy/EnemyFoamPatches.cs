81deead744d453033ac0c63c79ac1ebf871a2f3c
Dinorush <onparnorthstar@gmail.com>
2025-01-05 13:16:16 -0500

    Remove unnecessary glue damage fix
    Revamped Foam system no longer has a need to use Damageable.GlueDamage, and this fix modifies C-Foam tripmine behavior, so best to remove it

--- ExtraWeaponCustomization/Patches/Enemy/EnemyFoamPatches.cs
+++ ExtraWeaponCustomization/Patches/Enemy/EnemyFoamPatches.cs
@@ -6,18 +6,6 @@ namespace EWC.Patches.Enemy
     [HarmonyPatch]
     internal static class EnemyFoamPatches
     {
-        [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.ReceiveGlueDamage))]
-        [HarmonyPrefix]
-        private static bool FixGlueDamage(Dam_EnemyDamageBase __instance, pMiniDamageData data)
-        {
-            GlueVolumeDesc glueVolumeDesc = default;
-            glueVolumeDesc.volume = data.damage.Get(100f); // Epic .Get(HealthMax) when .Set(100f) is used
-            glueVolumeDesc.expandVolume = 0f;
-            glueVolumeDesc.currentScale = 0f;
-            __instance.AddToTotalGlueVolume(null, glueVolumeDesc);
-            return false;
-        }
-
         [HarmonyPatch(typeof(Dam_EnemyDamageBase), nameof(Dam_EnemyDamageBase.AddToTotalGlueVolume))]
         [HarmonyPriority(Priority.Low)]
         [HarmonyWrapSafe]
