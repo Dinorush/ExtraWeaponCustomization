29e0a68604709bc1510dc824a0f014273b11b7c0
Dinorush <onparnorthstar@gmail.com>
2025-02-23 20:11:14 -0500

    Add UniqueCap & ConsumeOnTrigger fields

--- ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerObjects/DamageableTrigger.cs
+++ ExtraWeaponCustomization/CustomWeapon/Properties/Effects/Triggers/TriggerObjects/DamageableTrigger.cs
@@ -2,6 +2,7 @@
 using EWC.CustomWeapon.ObjectWrappers;
 using EWC.CustomWeapon.WeaponContext.Contexts;
 using EWC.CustomWeapon.WeaponContext.Contexts.Triggers;
+using EWC.Utils.Log;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json;
@@ -11,6 +12,8 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
     public class DamageableTrigger<TContext> : DamageTypeTrigger<TContext> where TContext : WeaponHitDamageableContextBase
     {
         public float UniqueThreshold { get; private set; } = 0f;
+        public float UniqueCap { get; private set; } = 0f;
+        public bool UniqueConsumeOnTrigger { get; private set; } = true;
 
         private Dictionary<BaseDamageableWrapper, float>? _uniqueCounts;
         private static BaseDamageableWrapper TempWrapper => BaseDamageableWrapper.SharedInstance;
@@ -41,12 +44,13 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
                 }
                 _uniqueCounts[TempWrapper] += amount;
 
-                if (_uniqueCounts[TempWrapper] < UniqueThreshold)
+                if (_uniqueCounts[TempWrapper] < UniqueThreshold || (UniqueCap > 0f && _uniqueCounts[TempWrapper] > UniqueCap))
                 {
                     amount = 0f;
                     return true;
                 }
-                _uniqueCounts[TempWrapper] -= UniqueThreshold;
+                else if(UniqueConsumeOnTrigger)
+                    _uniqueCounts[TempWrapper] -= UniqueThreshold;
             }
 
             return true;
@@ -78,10 +82,18 @@ namespace EWC.CustomWeapon.Properties.Effects.Triggers
             {
                 case "uniquethreshold":
                     UniqueThreshold = reader.GetSingle();
-                    if (UniqueThreshold > 0)
-                        _uniqueCounts = new();
+                    break;
+                case "uniquecap":
+                    UniqueCap = reader.GetSingle();
+                    break;
+                case "uniqueconsumeontrigger":
+                case "uniquedecreaseontrigger":
+                    UniqueConsumeOnTrigger = reader.GetBoolean();
                     break;
             }
+
+            if (_uniqueCounts == null && (UniqueThreshold > 0 || UniqueCap > 0))
+                _uniqueCounts = new();
         }
     }
 }
