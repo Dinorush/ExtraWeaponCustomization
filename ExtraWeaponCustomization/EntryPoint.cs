5999645c48ad0e4303f541928db4033d770c5ea0
Dinorush <onparnorthstar@gmail.com>
2025-03-24 06:55:18 -0400

    Version bump

--- ExtraWeaponCustomization/EntryPoint.cs
+++ ExtraWeaponCustomization/EntryPoint.cs
@@ -16,7 +16,7 @@ using EWC.CustomWeapon.Properties.Traits.CustomProjectile.Components;
 
 namespace EWC;
 
-[BepInPlugin("Dinorush." + MODNAME, MODNAME, "3.0.0")]
+[BepInPlugin("Dinorush." + MODNAME, MODNAME, "3.0.2")]
 [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MTFOAPIWrapper.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(KillAPIWrapper.PLUGIN_GUID, BepInDependency.DependencyFlags.SoftDependency)]
