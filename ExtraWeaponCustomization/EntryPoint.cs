1419f9854311336ff9a0ce6bb65969e33ee69454
Dinorush <onparnorthstar@gmail.com>
2025-11-23 14:38:04 -0500

    Revert version num

--- ExtraWeaponCustomization/EntryPoint.cs
+++ ExtraWeaponCustomization/EntryPoint.cs
@@ -13,7 +13,7 @@ using System.Runtime.CompilerServices;
 
 namespace EWC;
 
-[BepInPlugin("Dinorush." + MODNAME, MODNAME, "4.2.8")]
+[BepInPlugin("Dinorush." + MODNAME, MODNAME, "4.2.7")]
 [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MTFOAPIWrapper.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MSAPIWrapper.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
