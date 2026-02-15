625af682842068ce0ee5689621ae7ccd666255b4
Dinorush <onparnorthstar@gmail.com>
2026-02-14 21:53:47 -0500

    Version bump

--- ExtraWeaponCustomization/EntryPoint.cs
+++ ExtraWeaponCustomization/EntryPoint.cs
@@ -13,7 +13,7 @@ using System.Runtime.CompilerServices;
 
 namespace EWC;
 
-[BepInPlugin("Dinorush." + MODNAME, MODNAME, "4.6.5")]
+[BepInPlugin("Dinorush." + MODNAME, MODNAME, "4.6.6")]
 [BepInDependency("dev.gtfomodding.gtfo-api", BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency(MTFOAPIWrapper.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
 [BepInDependency("Dinorush.ModifierAPI", BepInDependency.DependencyFlags.HardDependency)]
